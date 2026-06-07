820a11a241a9ecf8e99b254009dc80569b3a24e3
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-06-07 16:04:05 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] ch = new int[26];
+        for(int i = 0; i < s.length(); i++) 
+        {
+            ch[s.charAt(i) - 'a']++;
+            ch[t.charAt(i) - 'a']--;
+        }
+        for(int i : ch) if(i != 0) return false;
+        return true;
+    }
+}
