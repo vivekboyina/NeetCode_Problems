68e6c882943c86748f08baa9a53e787af3b707b1
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-03 12:11:41 +0530

    Add: maximum-number-of-balloons - submission-2

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char,int>mp;
+        for(char i : text) if(i =='b'|| i == 'a' || i == 'l' || i == 'n' || i == 'o') mp[i]+=1;
+        int ans = INT_MAX;
+        if(mp.size() < 5) return 0;
+        for(auto it : mp)
+        {
+            if(it.first == 'l' || it.first == 'o') ans = min(ans,it.second/2);
+            else ans = min(ans,it.second);
+        }
+        return ans;
+    }
+};
