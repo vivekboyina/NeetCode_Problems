27cf0c1d606fad4cc433e1760187f79310221e31
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-09 19:57:06 +0530

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        mp[0] = 1;
+        int sum = 0;
+        int ans = 0;
+        for(int i : nums)
+        {
+            sum+=i;
+            if(mp.count(sum - k)) ans+=mp[sum - k];
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
